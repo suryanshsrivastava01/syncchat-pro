1d5931d26129e2723912b7d7d4e18e133f8a5690
Suryansh Srivastava <rs876252@gmail.com>
2026-06-21 10:12:12 +0530

    Fix port for Render

--- server.js
+++ server.js
@@ -120,15 +120,13 @@ io.on("connection", (socket) => {
 
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 server.listen(
     PORT,
     () => {
-
         console.log(
-            `Server running on http://localhost:${PORT}`
+            `Server running on port ${PORT}`
         );
-
     }
 );
